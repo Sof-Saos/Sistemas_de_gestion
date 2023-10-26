fd303673ec71f81533a9c3d7ebe69c198a0572cc
Sof Salamanca <74073693+Sof-Saos@users.noreply.github.com>
2023-10-26 07:31:10 -0500

    Add files via upload

---
+++ habitaciones/habitaciones/src/main/java/com/gestion/habitaciones/HabitacionesApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.habitaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitacionesApplication.class, args);
+	}
+
+}
